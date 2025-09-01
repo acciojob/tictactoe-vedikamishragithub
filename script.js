a631e4f9fe881678b578c18d6290cd18a7f124a9
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-09-02 00:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListner('click'()=>{
+      const nameX = document.getElementById('player-1').value.trim();
+      const nameO = document.getElementById('player-2').value.trim();
+	if(!nameO || !nameX){
+     alert('Please enter both name');
+	 return	
+	}
+	      players.X = nameX;
+      players.O = nameO;
+
+      formContainer.style.display = 'none';
+      gameContainer.style.display = 'block';
+
+      startTicTacToe();
+});
